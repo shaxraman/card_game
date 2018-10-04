3d0075a952c117db6217ff6ff819b3849a144b44
Nezamov Stanislav <shaxraman@gmail.com>
2018-10-04 13:37:39 +0300

    начало в pygame

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("hello word")
+import pygame, sys
+
+pygame.init()
+
+pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Cards..... Jack")
+while 1:
+    for event in pygame.event.get():
+        15
+        if event.type == pygame.QUIT:
+            print('Кнопка выхода нажата\nBuy')
+            sys.exit()
