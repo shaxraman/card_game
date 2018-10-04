02535469d795614695a570aed99d1ea42edbfc57
Nezamov Stanislav <shaxraman@gmail.com>
2018-10-04 15:19:03 +0300

    квадратики

--- first.py
+++ first.py
@@ -1,12 +1,35 @@
-import pygame, sys
+import pygame
 
 pygame.init()
-
-pygame.display.set_mode((800, 600))
+game_work = True
+# region color
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+# endregion
+# region Экран
+window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Cards..... Jack")
-while 1:
+# endregion
+
+# Делаем квадрат и его координаты
+screen = pygame.Surface((40, 40))
+screen.fill(RED)
+
+myfont = pygame.font.SysFont('Comic Sans MS', 8)
+textsurface = myfont.render('jopa', False, (0, 0, 0))
+
+
+while game_work:
+    # region выход из игры
     for event in pygame.event.get():
-        15
         if event.type == pygame.QUIT:
-            print('Кнопка выхода нажата\nBuy')
-            sys.exit()
+            print('Кнопка выхода нажата\nBuy it game')
+            game_work = False
+    # endregion
+
+    # Надпись в квадрате
+    screen.blit(textsurface, (0, 0))
+    window.blit(screen, (40 - 10, 600 - 40 - 10))
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from binar import *
 
 create_coloda()
 
-while True:
+game_not_over = True
+while game_not_over:
     a = input("Что вы хотите сделать?\n	1-получит карту? \n	2- положить карту на стол ")
     if a == "1":
         give_card(6)
     elif a == '2':
         put_on_table()
     else:
-        print('buy')
-        break
+        print('Buy it game')
+        game_not_over = False
