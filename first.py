c2fa91f9576aa9c57ddb6bdc8db10639c8674b63
Nezamov Stanislav <shaxraman@gmail.com>
2018-10-05 15:42:37 +0300

    better + coloda cards

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 from binar import *
 
 create_coloda()  # ================================================ Делаем колоду
@@ -11,16 +11,16 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 # endregion
 # region Экран
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((1820, 900))
 pygame.display.set_caption("Cards..... Jack")
 # endregion
-col_x = 10
-col_y = 0
+col_x = 15
+col_y = 50
 # Делаем карту, цвет и ее координаты
 card = pygame.Surface((60, 80))
 card.fill(WHITE)
 
-
+ab = 400
 def text_in_box(text='Jopa', size=10):
     myfont = pygame.font.SysFont('Comic Sans MS', size)
     textsurface = myfont.render(text, False, BLACK)
@@ -41,16 +41,20 @@ while game_work:
     if flag:
         for i in coloda:
             b = i
+            # i = pygame.Surface((65, 95)), pygame.Surface
+            # i.fill(RED)
             i = pygame.Surface((60, 80))
             i.fill(WHITE)
             i.blit(text_in_box(b), (0, 0))
             window.blit(i, (col_x, col_y))
-            if col_x <= 400:
-                col_x += 15
-                col_y += 15
+            if col_y <= ab:
+                col_x += 24
+                col_y += 50
+                ab = 400
             else:
-                col_x += 15
-                col_y -= 15
+                col_x += 24
+                col_y -= 50
+                ab = 50
     flag = False
     # card.blit(text_in_box(), (0, 0))
     # window.blit(card, (col_x, col_y))
